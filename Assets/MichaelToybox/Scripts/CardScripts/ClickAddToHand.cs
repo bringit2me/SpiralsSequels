3abdbf0039af39a1d2df2404a5f31c52dc57a728
MichaellMcD <58204837+MichaellMcD@users.noreply.github.com>
2023-12-01 15:47:10 -0500

    Removing test code from ClickAddToHand
    Removing test code from ClickAddToHand
    Updating MLM_Toybox

--- Assets/MichaelToybox/Scripts/CardScripts/ClickAddToHand.cs
+++ Assets/MichaelToybox/Scripts/CardScripts/ClickAddToHand.cs
@@ -11,26 +11,7 @@ public class ClickAddToHand : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0) == false) //Player did not Left click
-        {
-            PointerEventData pointerData_ = new PointerEventData(EventSystem.current) //gets pointer data
-            {
-                pointerId = -1,
-            };
-
-            pointerData_.position = Input.mousePosition; //sets pointer data position
-            List<RaycastResult> results_ = new List<RaycastResult>(); //list of raycast results
-            EventSystem.current.RaycastAll(pointerData_, results_); //sets list of raycast results to everything under the mouse
-
-            foreach (RaycastResult result in results_)
-            {
-                if (result.gameObject == this.gameObject)
-                {
-                    Debug.Log("Hovering: " + card.name);
-                    return;
-                }
-            }
             return;
-        }
 
         PointerEventData pointerData = new PointerEventData(EventSystem.current) //gets pointer data
         {
